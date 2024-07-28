cfa85137437715ffb9334c716b2b87808b2b9744
svskaushik <shaurya9702@gmail.com>
2024-07-28 15:13:43 -0600

    Added author profile information and related paper details to the Paper interface.

--- app/types.ts
+++ app/types.ts
@@ -3,10 +3,19 @@ export interface Paper {
   title: string;
   summary: string;
   abstract: string;
-  authors: string[];
+  authors: Author[];
   link: string;
+  pdfLink: string;
   categories: string[];
   published: string;
   updated: string;
   doi: string | null;
+  relatedPapers: Paper[];
+  citationCount: number;
+  altmetric: number;
+}
+
+export interface Author {
+  name: string;
+  profileUrl: string;
 }
